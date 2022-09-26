27f2a59f7759e2fb8bc16717addcd04bed24a4c6
john_kevin.campanilla@student.passerellesnumeriques.org <rdoclara@gmail.com>
2022-09-26 08:15:14 +0800

    declared arraylist and created a menu function

--- ArrayList_Implementation.java
+++ ArrayList_Implementation.java
@@ -1,5 +1,32 @@
+import java.util.*;
+
+import javax.xml.transform.Source;
 public class ArrayList_Implementation{
+    static Scanner input = new Scanner(System.in);
+
+
     public static void main (String[] args){
+        ArrayList<Integer> arrList = new ArrayList<Integer>(1); //declaring an arraylist
+        String response = input.nextLine();
+
+
+
+    }
+    
+
+    public void menu(String response){
+        boolean end = false;
+        System.out.println("""
+                SELECT FROM THE FOLLOWING CHOICES:
+                A. ADD NUMBER
+                B. REMOVE NUMBER
+                C. DISPLAY NUMBER
+                D. CLEAR NUMBERS
+                E. EXIT
+                """);
+        
+         
+
 
     }
 }
